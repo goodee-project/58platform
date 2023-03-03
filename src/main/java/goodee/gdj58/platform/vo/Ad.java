1a5f951b910e470b1094414694e1948a17de29aa
AndreaJo1201 <dexember1201@gmail.com>
2023-03-03 15:48:08 +0900

    [조건희] VO 생성(AD)

---
+++ src/main/java/goodee/gdj58/platform/vo/Ad.java
@@ -0,0 +1,17 @@
+package goodee.gdj58.platform.vo;
+
+import lombok.Data;
+
+@Data
+public class Ad {
+	private int adNo;
+	private String adCustomerId;
+	private String adUrl;
+	private int adPrice;
+	private String adState;
+	private String adPosition;
+	private String adStartdate;
+	private String adEnddate;
+	private String adDuration;
+	private String createdate;
+}
---
+++ src/main/java/goodee/gdj58/platform/vo/AdCustomer.java
@@ -0,0 +1,10 @@
+package goodee.gdj58.platform.vo;
+
+import lombok.Data;
+
+@Data
+public class AdCustomer {
+	private String adCustomerId;
+	private String adCustomerPw;
+	private String adCustomerName;
+}
