4a4b20c58e8a033c35cd29bc961a25acb312eaac
nomad-lee <homi_yi@naver.com>
2023-03-06 13:58:33 +0900

    [이호민] 기업관리 관련 파일 생성, 기업 쿼리 작성중
    - CompanyListController : 기업목록 조회
    - CompanyOneController : 한 기업 상세정보
    - CompanySignUpCheckController : 기업 가입승인 목록 및 승인
    - DeleteCompanyListController : 탈퇴 기업 목록
    - UpdateCompanyListController : 정보수정 요청 기업 목록
    - UpdateCompanyOneController : 정보수정 요청내용 확인 및 승인
    
    - CompanyService, CompanyMapper 생성
    - 컨트롤러 별 jsp 생성

---
+++ src/main/java/goodee/gdj58/platform/controller/company/UpdateCompanyListController.java
@@ -0,0 +1,5 @@
+package goodee.gdj58.platform.controller.company;
+
+public class UpdateCompanyListController {
+
+}
---
+++ src/main/java/goodee/gdj58/platform/mapper/CompanyMapper.java
@@ -0,0 +1,5 @@
+package goodee.gdj58.platform.mapper;
+
+public class CompanyMapper {
+
+}
